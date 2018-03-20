9a5450269fd0ab1e84160ab3f904c85baac265ae
Rick G <richkws@gmail.com>
2018-03-20 22:54:40 +0000

    Implement destroy action for lists and users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -17,6 +17,17 @@ class Api::UsersController < ApiController
     render json: users, each_serializer: UserSerializer
   end
   
+  def destroy
+    begin 
+      user = User.find(params[:id])
+      user.destroy
+      
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
+  end
+  
   private
   def user_params
     params.require(:user).permit(:name, :password)
